abd71514f79e09d09b50620b7b381a90737eebeb
chatta2539 <chatta2539@gmail.com>
2020-01-10 16:14:01 +0700

    B

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var http = require("https");
+const request = require('request');
+const app = express() // สร้างตัวแปร app เป็น instance ของ express app
+const path = require('path')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('login', { title: 'Express' });
 });
+
+router.get('/index2', function(req, res, next) {
+    httpgetWeather().then(function(result) {
+        res.render('index', { title: 'Express', timeZone: result });
+        // res.write(JSON.stringify([1, 2, 3, result]));
+        // res.end();
+
+    });
+});
+router.get('/login', function(req, res, next) {
+    res.render('login');
+});
+
+function httpgetWeather() {
+    return new Promise(function(resolve, reject) {
+        request('https://api.darksky.net/forecast/049312bbd468e56d6dc7c85dfa647397/13.5528828,100.7910837', { json: true }, (err, res, body) => {
+            if (err) { return console.log(err); }
+            console.log(body.timezone);
+            resolve(body.timezone);
+        });
+
+    });
+}
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET users listing. */
+var http = require("https");
+const request = require('request');
+const app = express() // สร้างตัวแปร app เป็น instance ของ express app
+const path = require('path')
+    /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
+
+router.get('/index2', function(req, res, next) {
+    httpgetWeather().then(function(result) {
+        res.render('index', { title: 'Express', timeZone: result });
+        // res.write(JSON.stringify([1, 2, 3, result]));
+        // res.end();
+
+    });
+});
+
+function httpgetWeather() {
+    return new Promise(function(resolve, reject) {
+        request('https://api.darksky.net/forecast/049312bbd468e56d6dc7c85dfa647397/13.5528828,100.7910837', { json: true }, (err, res, body) => {
+            if (err) { return console.log(err); }
+            console.log(body.timezone);
+            resolve(body.timezone);
+        });
+
+    });
+}
 
 module.exports = router;
